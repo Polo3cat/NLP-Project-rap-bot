98b501e2163866eb43f5e7c81bcda5a25ef48514
Pablo Carbonell <polo3ies@gmail.com>
2018-10-10 19:35:14 +0200

    Add error control

--- rapper.py
+++ rapper.py
@@ -8,7 +8,17 @@ import requests
 import re
 
 
+class NoRhyme(BaseException):
+    pass
+
+
+class Empty(BaseException):
+    pass
+
+
 def find_similar_with_tag(word, w_pos, obj_pos, posteriori_map, candidates):
+    if not candidates:
+        raise Empty
     for w in candidates:
         if w != word:
             ret = w
@@ -31,6 +41,8 @@ def fetch_rhyme(token, pos):
         p = nltk.pos_tag([j['word']])
         if p[0][1] == pos:
             return p[0]
+    if not json:
+        raise NoRhyme
     return nltk.pos_tag([json[0]['word']])[0]
 
 
@@ -78,21 +90,31 @@ while True:
         tokens = list(filter(lambda x: re.fullmatch(r'\w+', x), tokens))
         ttags = nltk.pos_tag(tokens)
         tags = tuple([x[1] for x in ttags])
+        if not sample or not ttags or not tags:
+            print('(Info.) You forgot to write something!')
+            continue
         if tags not in grammar:
             answer_grammar = find_similar_grammar(tags, grammar.keys())
         else:
-            answer_grammar = grammar[tags][:-1]  # we ignore the last since we got it from the rhyme
-        rhyme = fetch_rhyme(ttags[-1][0], answer_grammar[-1])
+            answer_grammar = grammar[tags]
+        try:
+            rhyme = fetch_rhyme(ttags[-1][0], answer_grammar[-1])
+        except NoRhyme as e:
+            print("(Info.) Couldn't find a rhyme for that!")
+            continue
         answer = [rhyme[0]]
         previous = rhyme[0]
         prev_pos = rhyme[1]
-        for tag in reversed(answer_grammar[:-1]):
-            if previous not in mapping or tag not in mapping[previous]:
-                previous = find_similar_with_tag(previous, prev_pos, tag, mapping, types_by_pos[tag])
-            else:
-                previous = mapping[previous][tag]
-            answer += [previous]
-            prev_pos = tag
+        try:
+            for tag in reversed(answer_grammar[:-1]):
+                if previous not in mapping or tag not in mapping[previous]:
+                    previous = find_similar_with_tag(previous, prev_pos, tag, mapping, types_by_pos[tag])
+                else:
+                    previous = mapping[previous][tag]
+                answer += [previous]
+                prev_pos = tag
+        except Empty as e:
+            print("(Warning!) Model too small!")
         print(" ".join(reversed(answer)))
     except EOFError as e:
         print()
