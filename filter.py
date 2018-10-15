b02b829c631fa1f22fc014e7e024adc9992589d7
Pablo Carbonell <polo3ies@gmail.com>
2018-10-15 15:56:17 +0200

    Filter writes sentences

--- filter.py
+++ filter.py
@@ -8,14 +8,12 @@ source = sys.argv[1]
 output = sys.argv[2]
 genre  = sys.argv[3]
 amount = int(sys.argv[4])
-with open(source, newline='') as source_csv, open(output, newline='', mode='w') as output_csv:
+with open(source, newline='') as source_csv, open(output, newline='', mode='w') as output_f:
     csvreader = csv.DictReader(source_csv, dialect='unix')
-    csvwriter = csv.DictWriter(output_csv, dialect='unix', fieldnames=csvreader.fieldnames)
-    csvwriter.writeheader()
     count = 0
     for row in csvreader:
         if row['genre'] == genre:
-            csvwriter.writerow(row)
+            output_f.write(row['lyrics'])
             count += 1
         if count == amount:
             break
